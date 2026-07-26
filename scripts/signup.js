d9f0650881886b05625755ef1cc4f17f41d1b21c
Pashvinder <dhimanpashvinder@gmail.com>
2026-07-26 15:48:29 +0530

    passwordValidation and passwordMatch working

--- scripts/signup.js
+++ scripts/signup.js
@@ -2,9 +2,46 @@ const form = document.getElementById("signUpForm");
 const validpassword = document.getElementById("validPassword");
 
 
+// Enter password is valid or not
+function passwordValidation(password) {
+    const passLength = password.length;
+    let hasUpperCase = false;
+    let hasLowercase = false;
+    let hasNumber = false;
+    let hasSpecial = false;
+
+    if (passLength >= 8) {
+        for (let i = 0; i < passLength; i++) {
+            if (password[i] >= 'A' && password[i] <= 'Z') { hasUpperCase = true };
+            if (password[i] >= 'a' && password[i] <= 'z') { hasLowercase = true };
+            if (password[i] >= '0' && password[i] <= '9') { hasNumber = true };
+            if (!(password[i] >= 'A' && password[i] <= 'Z') && !(password[i] >= 'a' && password[i] <= 'z') && !(password[i] >= '0' && password[i] <= '9')) {
+                hasSpecial = true;
+            }
+        }
+
+    }
+
+    if (passLength >= 8 && hasUpperCase === true && hasLowercase === true && hasNumber === true && hasSpecial === true) {
+        console.log("Valid Password");
+        return true;
+    }
+    else {
+        console.log("Invalid Password!");
+        alert("Password requirements not met.");
+        validpassword.innerText = "Requires: 1 Upper case, 1 Lower Case, 1 Number, 1 Special Chracter";
+        validpassword.style.color = "red";
+        return false;
+    }
+}
+
+
+
+// both passwords matches or not
 function passwordsMatch(password, confirmPass) {
     const unmatch = document.getElementById("unmatchedPassword")
     if (password !== confirmPass) {
+        console.log("Unmatched Passwords")
         unmatch.innerText = "Passwords do not match.";
         unmatch.style.color = "red";
         return false;
@@ -23,10 +60,16 @@ form.addEventListener("submit", function (e) {
     const password = document.getElementById("inPassword").value;
     const confirmPass = document.getElementById("inConfirm").value;
 
-
+    if (!passwordValidation(password)) {
+        return;
+    }
     if (!passwordsMatch(password, confirmPass)) {
         return;
     }
-    console.log("Passswords matched.")
+    else {
+        validpassword.innerText = "";
+        console.log("Passswords matched.")
+    }
+
 
 })
